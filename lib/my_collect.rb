df59c5ae143b27e1246cd4c598496d32e886367f
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-06-22 19:16:21 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
+def my_collect(array)
+  if block_given?
+    
+     i = 0 
+      while i < array.length 
+        yield(array[i])
+        i += 1 
+      end 
+      
+      return array  
+  else 
+    puts "Hey! No block was given!"
+  end 
+end 
 
